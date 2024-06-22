4c237e60317c4425a635cc18ae1bf6dbeec68dfa
KummariguntlaJahnavi <kummariguntlasaijahnavi@gmail.com>
2024-06-22 17:11:55 +0530

    new compomnent created unnecessarily

---
+++ src/app/headers/headers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headers',
+  standalone: true,
+  imports: [],
+  templateUrl: './headers.component.html',
+  styleUrl: './headers.component.scss'
+})
+export class HeadersComponent {
+
+}
