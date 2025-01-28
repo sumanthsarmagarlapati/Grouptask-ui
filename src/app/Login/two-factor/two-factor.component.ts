23aba7ba5b482e1bdb34b2405131219840e52cf0
Sumanth Sarma <sumanthsarma1306@gmail.com>
2025-01-28 08:40:38 +0530

    feat : modified routes logic

--- src/app/Login/two-factor/two-factor.component.ts
+++ src/app/Login/two-factor/two-factor.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-two-factor',
   templateUrl: './two-factor.component.html',
   styleUrls: ['./two-factor.component.scss']
--- src/app/routes/route-menus.ts
+++ src/app/routes/route-menus.ts
@@ -1,4 +1,4 @@
-export class ROuterMenus {
+export class RouterMenus {
   menus = {
     dashboard: {},
     login: {},
